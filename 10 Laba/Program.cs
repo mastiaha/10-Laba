097000f2a77b302f1ce9ad549bc4c8e841378383
mastiaha <mastiaha@mail.ru>
2022-01-03 20:38:55 +0300

    Добавьте файлы проекта.

---
+++ 10 Laba/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Laba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Мурка", "черный",1);
+            cat.Say();
+            
+            //Cat cat = new Cat("Мурка", "черный", 1);
+            //cat.Say();
+            //Console.WriteLine(cat.color);
+            Console.ReadKey();
+        }
+    }
+
+    class Cat
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        int age;
+
+        //public Cat(string name, string color = "Серый", int age = -1)
+        //{
+        //    this.name = name;
+        //    this.color = color;
+        //    this.age = age;
+        //}
+
+        public int Age
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    age = value;
+                }
+
+                else
+                {
+                    Console.WriteLine("Возраст не может быть отрицательным");
+                }
+            }
+            get
+            {
+                return age;
+            }
+        }
+        
+
+        //public void SetAge (int age)
+        //{
+        //    if (age > 0)
+        //        this.age = age;
+        //}
+        //public int GetAge ()
+        //{
+        //    return age;
+        //}
+        public Cat(string name, string color, int age)
+        {
+            Name = name;
+            Color = color;
+            Age = age;
+        }
+        public void Say()
+        {
+            Console.WriteLine("Мяу. Меня зовут {0}. У меня {1} цвет. Мне {2} лет", Name, Color, Age);
+        }
+    }
+}
---
+++ Задача 10/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ugol ugol = new Ugol(5, 95, 99);
+            ugol.Gradus();
+            ugol.ToRadians();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Ugol
+    {
+        int grad { get; set; }
+        int min { get; set; }
+        int sec { get; set; }
+
+       
+        public int Grad
+        {
+            set
+            {
+                grad = value;
+            }
+            get
+            {
+                return grad;
+            }
+        }
+        public int Min
+        {
+            set
+            {
+                min = value % 60;
+                grad += value / 60;
+            }
+            get
+            {
+                return min;
+            }
+        }
+        public int Sec
+        {
+            set
+            {
+                sec = (value % 60) % 60;
+                min += value / 60;
+            }
+            get
+            {
+                return sec;
+            }
+        }
+
+        public Ugol(int grad, int min, int sec)
+        {
+            Grad = grad;
+            Min = min;
+            Sec = sec;
+        }
+        public void Gradus()
+        {
+            Console.WriteLine("Дан угол {0} градусов {1} минут {2} секунд", Grad, Min, Sec);
+        }
+        public void ToRadians()
+        {
+            double Radians = (grad + min / 60 + sec / 360) * (Math.PI) / 180;
+            Console.WriteLine("В радианах соответственно {0} рад", Radians);
+        }
+        
+    }
+}
